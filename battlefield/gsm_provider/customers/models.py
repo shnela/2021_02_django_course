945b46ec4a1b9b1bb43434b1a8598cca32d9d160
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2021-03-01 00:05:46 +0100

    DB connection - simple models - Solved

--- battlefield/gsm_provider/customers/models.py
+++ battlefield/gsm_provider/customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    INDIVIDUAL = 'IND'
+    BUSINESS = 'BUS'
+    TYPE_CHOICES = [
+        (INDIVIDUAL, 'Individual'),
+        (BUSINESS, 'Business'),
+    ]
+    type = models.CharField(
+        max_length=3,
+        choices=TYPE_CHOICES,
+        default=INDIVIDUAL,
+    )
+    username = models.CharField(max_length=128, unique=True)
--- battlefield/gsm_provider/gsm_provider/settings.py
+++ battlefield/gsm_provider/gsm_provider/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
+    'billings',
+    'customers',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'gsm_provider.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
