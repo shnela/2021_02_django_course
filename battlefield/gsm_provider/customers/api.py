8636b84a556af823a8dcb7b28043bc01ddddfa39
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2021-03-02 03:24:43 +0100

    Permissions - solution

--- battlefield/gsm_provider/customers/api.py
+++ battlefield/gsm_provider/customers/api.py
@@ -2,13 +2,16 @@ from rest_framework import generics
 
 from customers.models import Customer
 from customers.serializers import CustomerSerializer
+from rest_framework import permissions
 
 
 class CustomerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
--- battlefield/gsm_provider/customers/tests/tests_api.py
+++ battlefield/gsm_provider/customers/tests/tests_api.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from customers.models import Customer
-from rest_framework.test import APIClient, APIRequestFactory
+from rest_framework.test import APIClient
 
 
 class CustomerAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.user = User.objects.create(username='someone', password='pass')
 
     def test_create(self):
+        self.client.force_authenticate(user=self.user)
         request = self.client.post('/api/customers/', {'username': 'Ada'})
         self.assertEqual(request.status_code, 201)
 
