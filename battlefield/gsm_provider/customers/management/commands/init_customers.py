921373ef03e9713c4399a6964bf9e5213f27cd37
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2021-03-03 12:54:10 +0100

    Revert "Tabula rasa"
    This reverts commit 239731b4

---
+++ battlefield/gsm_provider/customers/management/commands/init_customers.py
@@ -0,0 +1,24 @@
+from random import choice
+
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from customers.models import Customer
+
+fake = Faker()
+
+
+class Command(BaseCommand):
+    help = 'Initializes Customer model'
+
+    def add_arguments(self, parser):
+        parser.add_argument('customers_no', type=int)
+
+    def handle(self, *args, **options):
+        customers_to_add = list()
+        for _ in range(options['customers_no']):
+            customers_to_add.append(
+                Customer(username=fake.unique.first_name(),
+                         type=choice(Customer.TYPE_CHOICES)[0])
+            )
+        Customer.objects.bulk_create(customers_to_add)
