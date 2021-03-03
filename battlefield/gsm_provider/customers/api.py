a299cf8dd8e892c443d546be7b5f2326653ec377
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2021-03-04 00:36:43 +0100

    Start API 2

--- battlefield/gsm_provider/customers/api.py
+++ battlefield/gsm_provider/customers/api.py
@@ -1,24 +1,39 @@
+from rest_framework import mixins, generics
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from customers.models import Customer
+from customers.serializers import DetailCustomerSerializer, ListCustomerSerializer
 
-fake_customer = {'username': 'Olo'}
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def customer_list(request):
-    customers = Customer.objects.all()
-    fake_customers = [fake_customer for _ in range(10)]
-    return Response({
-        'customers': fake_customers
-    })
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = ListCustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = DetailCustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def customer_details(request, customer_id):
     customer = get_object_or_404(Customer, id=customer_id)
-    return Response({
-        'customer': customer
-    })
+    if request.method == 'GET':
+        serializer = DetailCustomerSerializer(customer)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DetailCustomerSerializer(customer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=204)
