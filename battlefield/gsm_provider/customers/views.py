239731b49c0cbf7d02945504a81c7d6ef55ab1c5
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2021-03-03 08:52:56 +0100

    Tabula rasa

--- battlefield/gsm_provider/customers/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("""
-                        <h1>Hi there.</h1>
-                        <a href='https://www.youtube.com/watch?v=4ctK1aoWuqY'>Schwifty response</a>
-                        """)
